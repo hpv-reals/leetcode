0df38b4841eb8e1ef4a782a6971f8dff7ade53a4
hiep.pham <phamvanhiep@weeds.co.kr>
2026-07-06 11:26:27 +0700

    06-07-2026

--- src/main/java/vn/com/leetcode/heap_priority_queue/KthLargest.java
+++ src/main/java/vn/com/leetcode/heap_priority_queue/KthLargest.java
@@ -37,6 +37,39 @@ public class KthLargest {
         return minHeap.peek();
     }
 
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<int[]> priorityQueue = new PriorityQueue<>((a, b) -> (b[0] * b[0] + b[1] * b[1]) - (a[0] * a[0] + a[1] * a[1]));
+
+        for (int[] point : points) {
+            priorityQueue.add(point);
+
+            if (priorityQueue.size() > k) {
+                priorityQueue.poll();
+            }
+        }
+
+        int[][] result = new int[k][2];
+        for (int i = 0; i < k; i++) {
+            result[i] = priorityQueue.poll();
+        }
+        return result;
+    }
+
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(k);
+        for (int num : nums) {
+            if (priorityQueue.isEmpty() || priorityQueue.size() < k) {
+                priorityQueue.add(num);
+            } else {
+                if (num > priorityQueue.peek()) {
+                    priorityQueue.poll();
+                    priorityQueue.add(num);
+                }
+            }
+        }
+        return priorityQueue.isEmpty() ? -1 : priorityQueue.poll();
+    }
+
     public int lastStoneWeight(int[] stones) {
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         for (int num : stones) {
