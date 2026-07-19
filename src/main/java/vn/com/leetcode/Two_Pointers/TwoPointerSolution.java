16102e20098fb7ff6f8f2b2321c69aedf134fdab
hpv <hiepkarate@gmail.com>
2026-07-19 19:38:21 +0700

    19-07-2026

--- src/main/java/vn/com/leetcode/Two_Pointers/TwoPointerSolution.java
+++ src/main/java/vn/com/leetcode/Two_Pointers/TwoPointerSolution.java
@@ -43,6 +43,48 @@ public class TwoPointerSolution {
         return true;
     }
 
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int x = word1.length() - 1;
+        int y = word2.length() - 1;
+        int length = Math.max(x, y);
+        for (int i = 0; i <= length; i++) {
+            if (i <= x) {
+                stringBuilder.append(word1.charAt(i));
+            }
+            if (i <= y) {
+                stringBuilder.append(word2.charAt(i));
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+    }
+
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+
     public static void main(String[] args) {
         TwoPointerSolution solution = new TwoPointerSolution();
 
