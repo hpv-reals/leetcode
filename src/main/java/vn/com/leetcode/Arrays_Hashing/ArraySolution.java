097c84aeaa6889edd1bdc9332e5fac71faee3f9c
hpv <hiepkarate@gmail.com>
2026-08-17 00:16:22 +0700

    16-08-2026

--- src/main/java/vn/com/leetcode/Arrays_Hashing/ArraySolution.java
+++ src/main/java/vn/com/leetcode/Arrays_Hashing/ArraySolution.java
@@ -164,6 +164,32 @@ public class ArraySolution {
 //        return list;
 //    }
 
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+
+        int i = 0;
+        while (i < n) {
+            int correctIndex = nums[i] - 1;
+
+            if (nums[i] > 0 && nums[i] <= n && nums[i] != nums[correctIndex]) {
+                int temp = nums[i];
+                nums[i] = nums[correctIndex];
+                nums[correctIndex] = temp;
+            } else {
+                i++;
+            }
+        }
+
+
+        for (int j = 0; j < n; j++) {
+            if ( nums[j] != j + 1) {
+                return j + 1;
+            }
+        }
+
+        return n + 1;
+    }
+
     public static void main(String[] args) {
         ArraySolution solution = new ArraySolution();
         int[] array  =new int[]{1,2,3,4,5};
